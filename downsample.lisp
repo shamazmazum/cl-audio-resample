a5c17fa499d04d396bc93021b334f66af2c10d92
Vasily Postnicov <shamaz.mazum@gmail.com>
2019-02-25 20:00:59 +0300

    full-blown resampler, which can up/downsample by any ratio. Also reduce calculations to single float

--- downsample.lisp
+++ downsample.lisp
@@ -1,31 +1,45 @@
 (in-package cl-audio-downsample)
 
-(declaim (type (double-float 0d0 1d0) *cutoff* *transition*))
-(defvar *transition* (/ 7d0)
+(deftype range-01 () '(single-float 0.0 1.0))
+(deftype positive-fixnum () '(integer 1 #.most-positive-fixnum))
+
+(declaim (type range-01 *cutoff* *transition*)
+         (type positive-fixnum *integration-steps*)
+         (type single-float +single-pi+))
+(defvar *transition* (/ 15.0)
   "Width of transition region, must be < 1d0. Values from 1/20 to 1/7 are good.
 Smaller values of *TRANSITION* require a filter of higher order.")
-(defvar *cutoff* (/ 2d0)
+(defvar *cutoff* (/ 2.0)
   "How much of the original band to keep? Varies from 0d0 to 1d0")
+(defparameter *integration-steps* 250
+  "Number of integration steps in filter fourier decomposition")
+(defconstant +single-pi+ (float pi 0.0)
+  "Single float pi constant")
+(defvar *filter-bank-table* (make-hash-table :test #'equalp)
+  "Filter bank memo")
 
-(declaim (ftype (function (double-float &optional) double-float)
+(declaim (ftype (function (range-01 &optional) range-01)
                 transition-poly
+                transition-poly-fast
                 transition-trig-poly)
-         (type (function (double-float &optional) double-float)
+         (type (function (range-01 &optional) range-01)
                *transition-function*))
 
 (defun transition-trig-poly (w)
   "Transition region of low-pass filter, trigonometric polynomial variant"
-  (declare (type double-float w))
+  (declare (type range-01 w)
+           (optimize (speed 3)))
   (/
    (+
-    (* -1 (cos (* 3 pi w)))
-    (* 9 (cos (* pi w)))
+    (* -1 (cos (* 3 +single-pi+ w)))
+    (* 9 (cos (* +single-pi+ w)))
     8)
    16))
 
 (defun transition-poly (w)
-  "Transition region of low-pass filter, polynomial variant"
-  (declare (type double-float w))
+  "Transition region of low-pass filter, better convergence polynomial variant"
+  (declare (type range-01 w)
+           (optimize (speed 3)))
   (+
    (* 20 (expt w 7))
    (* -70 (expt w 6))
@@ -33,14 +47,25 @@ Smaller values of *TRANSITION* require a filter of higher order.")
    (* -35 (expt w 4))
    1))
 
+(defun transition-poly-fast (w)
+  "Transition region of low-pass filter, faster calculation polynomial variant"
+  (declare (type range-01 w)
+           (optimize (speed 3)))
+  (+
+   (* 2 (expt w 3))
+   (* -3 (expt w 2))
+   1))
+
 (defvar *transition-function* #'transition-poly
   "Which transition function to use?")
 
 (defun integrate-function (func min max)
   "Integrate function f(x) from x = min to x = max"
-  (declare (type double-float min max)
-           (type (function (double-float &optional) double-float) func))
-  (let ((delta 1d-4)) ; Must depend on n really
+  (declare (type single-float min max)
+           (type (function (single-float &optional) single-float) func)
+           (optimize (speed 3)))
+  (let ((delta (float (/ *integration-steps*) 0.0)))
+    (declare (type single-float delta))
     (* (/ delta 6)
        (loop for x from min to max by delta sum
             (let ((x1 x)
@@ -48,69 +73,96 @@ Smaller values of *TRANSITION* require a filter of higher order.")
                   (x3 (+ x (/ delta 2))))
               (+ (funcall func x1)
                  (* 4 (funcall func x3))
-                 (funcall func x2)))))))
+                 (funcall func x2)))
+            single-float))))
 
 (defun get-filter-coeff (n)
   "N-th filter coefficient"
-  (declare (type integer n))
+  (declare (type (unsigned-byte 32) n)
+           (optimize (speed 3)))
   (if (zerop n) *cutoff*
       (flet ((integrand (w)
                (* (funcall *transition-function* w)
-                  (cos (* 2 pi n
+                  (cos (* 2 +single-pi+ n
                           (+ (* *cutoff* *transition* w)
                              (* *cutoff* (- 1 *transition*) 0.5)))))))
-        (+ (/ (sin (* *cutoff* (- 1 *transition*) n pi)) (* pi n))
+        (+ (/ (sin (* *cutoff* (- 1 *transition*) n +single-pi+)) (* +single-pi+ n))
            (* 2 *cutoff* *transition*
-              (integrate-function #'integrand 0d0 1d0))))))
+              (integrate-function #'integrand 0.0 1.0))))))
 
-(defun n-filter-coeffs (n)
-  (declare (type (integer 1) n))
-  (make-array n
-              :element-type 'double-float
-              :initial-contents
-              (loop for i below n collect
-                   (get-filter-coeff i))))
+(defun filter-bank (phases length)
+  "Calculate PHASESxLENGTH multiphase low-pass filter bank"
+  (declare (type unsigned-byte length phases))
+  (let* ((total-length (* phases length))
+         (max-idx (if (zerop (logand 1 total-length))
+                      (1- total-length) total-length))
+         (lin-len (/ (1- max-idx) 2))
+         (filter (make-array (list phases length)
+                             :element-type 'single-float)))
+    (loop for i below phases do
+         (loop for j below length do
+              (setf (aref filter i j)
+                    (let ((idx (+ (* j phases) i)))
+                      (if (< idx max-idx)
+                          (get-filter-coeff (abs (- idx lin-len)))
+                          0.0)))))
+    filter))
 
-(defun convolve (array flt &optional (drop-ratio 1))
-  "Convolve two sequences"
+(defstruct filter-bank-id
+  (phases 1 :type positive-fixnum)
+  (length 1 :type positive-fixnum)
+  (cutoff 0.0 :type single-float)
+  (transition 0.0 :type single-float))
+
+(defun get-filter-bank (phases length)
+  (let ((filter-id (make-filter-bank-id :phases phases
+                                        :length length
+                                        :cutoff *cutoff*
+                                        :transition *transition*)))
+  (multiple-value-bind (filter filter-found-p)
+      (gethash filter-id
+               *filter-bank-table*)
+    (cond
+      (filter-found-p filter)
+      (t (setf (gethash filter-id *filter-bank-table*)
+               (filter-bank phases length)))))))
+
+(defun resample-with-filtering (array up down filter-length)
+  "Perform audio resampling with low-pass filtering."
   (declare (optimize (speed 3))
-           (type (simple-array double-float) array flt)
-           (type (integer 1 #.most-positive-fixnum) drop-ratio))
-  (let* ((len1 (length array))
-         (len2 (1- (length flt)))
-         (res-len (floor len1 drop-ratio))
-         (new-array (make-array res-len
-                                :initial-element 0d0
-                                :element-type 'double-float)))
-    (declare (type (simple-array double-float) new-array))
-    (loop
-       for i fixnum from 0 below res-len
-       for idx1 fixnum from 0 by drop-ratio
-       do
-         (loop
-            for j fixnum from (- len2) to len2
-            for idx2 fixnum from 0 by 1 do
-              (let ((idx (- idx1 idx2)))
-                (incf (aref new-array i)
-                      (* (if (< idx 0) 0d0 (aref array idx))
-                         (aref flt (abs j)))))))
-    new-array))
+           (type (simple-array single-float (*)) array)
+           (type positive-fixnum up down filter-length))
+  (let* ((in-length (length array))
+         (out-length (floor (* in-length up) down))
+         (output (make-array out-length
+                             :element-type 'single-float
+                             :initial-element 0.0))
+         (filter (get-filter-bank up filter-length)))
+    (declare (type (simple-array single-float (* *)) filter))
+    (loop for i fixnum below out-length do
+         (multiple-value-bind (input-idx filter-idx)
+             (floor (* i down) up)
+           (declare (type (integer 0 #.most-positive-fixnum) input-idx filter-idx))
+           (setf (aref output i)
+                 (loop
+                    for j fixnum below filter-length
+                    for idx fixnum = (- input-idx j)
+                    sum
+                      (* up
+                         (aref filter filter-idx j)
+                         (if (< idx 0) 0.0 (aref array idx)))
+                      single-float))))
+    output))
 
-(defun downsample (array ratio &key (filter-order 50)
+(defun resample (array up down &key
+                                 (filter-length 250)
                                  (transition-function :poly)
                                  (transition *transition*))
-  "Downsample a signal ARRAY by a ratio RATIO. You can specify the
-filter order in FILTER-ORDER. You can use filters with two
-TRANSITION-FUNCTIONs: :POLY and :TRIG-POLY.  Also you can specify a
-transition region TRANSITION. Values about 1/10 are good."
-  (declare (type (simple-array (double-float)) array)
-           (type (integer 1) ratio)
-           (type (integer 1) filter-order)
-           (type (member :poly :trig-poly) transition-function))
-  (let ((*cutoff* (float (/ ratio) 0d0))
-        (*transition* transition)
+  (let ((*transition* transition)
+        (*cutoff* (float (/ (max up down)) 0.0))
         (*transition-function*
          (ecase transition-function
            (:poly #'transition-poly)
+           (:poly-fast #'transition-poly-fast)
            (:trig-poly #'transition-trig-poly))))
-    (convolve array (n-filter-coeffs filter-order) ratio)))
+    (resample-with-filtering array up down filter-length)))
